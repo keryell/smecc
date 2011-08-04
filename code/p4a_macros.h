64f1d980511ce98311b8493c0701d1fa1e579d62
lanore <vincent.lanore@ens-lyon.fr>
2011-08-04 15:54:09 +0200

    Better version of the macros. Tested with p4a_test.c

--- code/p4a_macros.h
+++ code/p4a_macros.h
@@ -1,12 +1,12 @@
 #ifndef P4A_MACROS_H
 #define P4A_MACROS_H
 
-#include <smecy.h>
+#include "smecy.h"
 
 #define P4A_MAX_STREAMING_BUFFERS 64
 #define P4A_MAX_STREAMING_BUFFER_SIZE 1024
 
-static p4a_next_stream = 0;
+static int p4a_next_stream = 0;
 DbLink p4a_streaming_buffers[P4A_MAX_STREAMING_BUFFERS];
 
 #define p4a_init_stream(stream)\
@@ -16,21 +16,15 @@ DbLink p4a_streaming_buffers[P4A_MAX_STREAMING_BUFFERS];
 }
 
 #define p4a_launch_stream(stream, node)\
-	pth_CreateProcess(((int (*)())__Node_##stream##_##node),__stream_buffer_##stream)
+	pth_CreateProcess(((int (*)())__Node_##stream##_##node))
 	
-#define p4a_arg(arg)\
-	struct_buffer->arg
-	
-#define p4a_def_begin_node(stream, node, condition, funccall)\
-void __Node_##stream##_##node(DbLink data_buffer)\
-{\
-    __buffer_type_##stream *struct_buffer; \
-    struct_buffer = ((__buffer_type_##stream *)(DbLinkGetInitBuf(data_buffer))); \
-    while(condition){ \
-      funcall; \
-      struct_buffer = ((__buffer_type_##stream *)(DbLinkPutData(data_buffer))); \
-    } \
-}
-
+#define p4a_stream_get_init_buf(stream)\
+    ((__buffer_type_##stream *)(DbLinkGetInitBuf(p4a_streaming_buffers[stream])))
+    
+#define p4a_stream_put_data(stream)\
+      struct_buffer = ((__buffer_type_##stream *)(DbLinkPutData(p4a_streaming_buffers[stream])))
+      
+#define p4a_stream_get_data(stream)\
+	struct_buffer = ((__buffer_type_##stream *)(DbLinkPutData(p4a_streaming_buffers[stream])));
 
 #endif
