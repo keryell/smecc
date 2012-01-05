a4c97f5388f40a31ae82917465f666d76937b49f
Ronan Keryell <Ronan.Keryell@hpc-project.com>
2012-01-05 09:24:42 +0100

    [SMECY] Add the source code for smecc_identity

---
+++ code/main_identity.cpp
@@ -0,0 +1,22 @@
+//small main for testing purposes
+
+#include "public.hpp"
+#include "smecyTranslation.hpp"
+
+int main(int argc, char *argv[])
+{
+	SgProject* project=frontend(argc,argv);
+
+	//generatePDF(*project);
+	//std::cerr << "Generating DOT" << std::endl;
+	//generateDOT(*project);
+
+	//TEST
+	//std::vector<SgFile*> files = project->get_fileList();
+	//for (unsigned int i=0; i<files.size(); i++)
+		//if (isSgSourceFile(files[i]))
+			//OmpSupport::lower_omp(isSgSourceFile(files[i]));
+
+	//AstTests::runAllTests(project);
+	return backend(project);
+}
