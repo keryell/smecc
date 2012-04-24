74d5257a266346502a8cf21fc359bc64bd259b0d
Ronan Keryell <Ronan.Keryell@wild-systems.com>
2012-04-24 16:25:16 -0700

    Change examples/input.c to cope with current SME-C compiler limitation
    It looks like only arrays are accepted to pass information between stages.

--- examples/input.c
+++ examples/input.c
@@ -19,17 +19,17 @@ void consume(int * thing) {
 
 
 int main() {
-  int b = sizeof(int);
+  int b[1] = { sizeof(int) };
   int c = 3;
 
 #pragma smecy stream_loop
   while (1) {
 #pragma smecy stream_node(1) arg(1,out) map(PE,1)
-    generate(&b);
+    generate(b);
 #pragma smecy stream_node(2) arg(1,inout)
-    transform(&b, c);
-#pragma smecy stream_node(2) arg(1,in)
-    consume(&b);
+    transform(b, c);
+#pragma smecy stream_node(3) arg(1,in)
+    consume(b);
   }
 
   return 0;
