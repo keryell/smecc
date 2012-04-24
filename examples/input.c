f37bb36c4fc2bdff23f40ef4e6225d037f749db0
Ronan Keryell <Ronan.Keryell@wild-systems.com>
2012-04-24 15:24:25 -0700

    Update the input.c example to have working code

--- examples/input.c
+++ examples/input.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 
-void stuff(int thing)
-{
-	
+/* Generate counting numbers */
+void generate(int * thing) {
+  static int state = 0;
+  *thing = state++;
 }
 
-void stuff2(int thing, int other_thing)
-{
-	
+
+/* Some basic transformation */
+void transform(int * thing, int product) {
+  *thing *= product;
 }
 
-int main()
-{
-	unsigned int tab[10][100];
-		
-	int b=sizeof(int);
-	int c=3;
-	#pragma smecy stream_loop
-	while (1)
-	{
-		#pragma smecy stream_node(1) arg(1,out) map(PE,1)
-			stuff(b);
-		#pragma smecy stream_node(2) arg(1,inout)
-			stuff2(b,c);
-		#pragma smecy stream_node(2) arg(1,in)
-			stuff(b);
-	}
-	
-	return 0;
+/* Consume the flux */
+void consume(int * thing) {
+  printf("%d ", *thing);
+}
+
+
+int main() {
+  int b = sizeof(int);
+  int c = 3;
+
+#pragma smecy stream_loop
+  while (1) {
+#pragma smecy stream_node(1) arg(1,out) map(PE,1)
+    generate(&b);
+#pragma smecy stream_node(2) arg(1,inout)
+    transform(&b, c);
+#pragma smecy stream_node(2) arg(1,in)
+    consume(&b);
+  }
+
+  return 0;
 }
