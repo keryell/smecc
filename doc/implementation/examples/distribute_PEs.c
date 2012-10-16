d4b4eb602e9444f862f14e4fe07a515319500d80
Ronan Keryell <Ronan.Keryell@wild-systems.com>
2012-10-16 16:40:31 -0700

    Add some design pattern examples for STHORM in the implementation guide

--- doc/implementation/examples/distribute_PEs.c
+++ doc/implementation/examples/distribute_PEs.c
@@ -7,9 +7,7 @@
 
 /* Initialize an array between 2 given lines */
 void init_array(int a[N][N], int begin, int end) {
-#pragma omp parallel for
   for (int i = begin; i < end; i++)
-#pragma omp parallel for
     for (int j = 0; j < N; j++)
       a[i][j] = 2*i + 3*j;
 }
@@ -18,18 +16,25 @@ int main() {
   int a[N][N];
 
   int slice = N/(NB_CLUSTERS*NB_PES);
-  // Launch enough OpenMP thread to control all the fabric:
-#pragma omp parallel for
+  /* Launch enough OpenMP thread to control all the fabric:
+
+     Assume that the runtime allows enough threads with nested
+     parallelism */
+#pragma omp parallel for num_threads(NB_CLUSTERS)
   for (int cluster = 0; cluster < NB_CLUSTERS; cluster++)
-#pragma omp parallel for
+#pragma omp parallel for num_threads(NB_PES)
     for (int pe = 0; pe < NB_PES; pe++) {
-      // Distribute the initialization on all the fabric
+      /* So now the iterations should be distributed with 1
+	 iteration/thread, on NB_CLUSTERS*NB_PES threads.
+
+	 Distribute the initialization on all the fabric: */
       int global_pe = cluster*NB_PES + pe;
       int begin = slice*global_pe;
       int end = MIN(N, slice*(global_pe + 1));
 #pragma smecy map(STHORM, cluster, pe)
 #pragma smecy arg(a, out, /[begin:end-1][])
       init_array(a, begin, end);
+    }
 
   printf("a[27][42] = %d\n", a[27][42]);
 
